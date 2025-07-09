4795525ac3087212167cb2bb3af4646c9ebf0f4f
ngohuy26 <79693723+ngohuyst26@users.noreply.github.com>
2025-07-09 15:18:03 +0700

    [FIX BUG] fix login to confirm you are not a bot with yt-dlp

--- src/services/puppeteerService.js
+++ src/services/puppeteerService.js
@@ -1,10 +1,7 @@
 const puppeteer = require("puppeteer");
 
 async function captureThumbnail(url, outputPath) {
-  const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    headless: "new",
-  });
+  const browser = await puppeteer.launch({ headless: "true" });
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: "networkidle2" });
   await page.waitForSelector("video");
